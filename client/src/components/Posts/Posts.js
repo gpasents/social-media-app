1e9fc63da7b12c88a0e03a30bcaa03e46f715942
Giorgos Pasentsis <gpasents@gmail.com>
2022-06-07 14:30:35 +0300

    init commit

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import Post from './Post/Post.js'
+
+import React from "react";
+
+const Posts = () => {
+    return (
+        <>
+            <h1> Posts </h1>
+            <Post/>
+            <Post/>
+        </>
+    )
+}
+
+export default Posts
