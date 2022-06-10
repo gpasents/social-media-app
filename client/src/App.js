0b183e7c24c207b5ce8a04729729867ffb332957
Giorgos Pasentsis <gpasents@gmail.com>
2022-06-10 20:21:17 +0300

    add styles

--- client/src/App.js
+++ client/src/App.js
@@ -8,29 +8,26 @@ import memories from './images/memories.png'
 
 import useStyles from './styles'
 const App = () => {
+    const classes = useStyles();
     return (
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align='center'>memories</Typography>
-                <img src={memories} alt='memories' height={60} />
-
-            </AppBar>
-
-            <Grow in>
-                <Container>
-                    <Grid container justify="space-between" alignItems='stretch' spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form />
-                        </Grid>
-                    </Grid>
-                </Container>
-
-            </Grow>
-
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
         </Container>
+      </Grow>
+    </Container>
     )
 }
 
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,8 +1,9 @@
 import Post from './Post/Post.js'
-
+import useStyles from './styles'
 import React from "react";
 
 const Posts = () => {
+    const classes = useStyles();
     return (
         <>
             <h1> Posts </h1>
