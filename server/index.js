97e7a146375fd254861dfded0466094729715cb8
Giorgos Pasentsis <gpasents@gmail.com>
2022-06-12 20:09:27 +0300

    add environmental variables

--- server/index.js
+++ server/index.js
@@ -2,20 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js'
 
 
 const app = express();
-
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use('/posts',postRoutes)
+app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://admin:adminpassword@cluster0.veskk.mongodb.net/MernProject?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
