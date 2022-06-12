d99f4154efd980f6506ff65d455fea692a4bc6cc
Giorgos Pasentsis <gpasents@gmail.com>
2022-06-12 17:31:53 +0300

    add like functionality

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -8,14 +8,14 @@ import { useDispatch } from 'react-redux';
 
 import useStyles from './styles'
 
-import { deletePost } from '../../../actions/posts';
+import { deletePost,likePost } from '../../../actions/posts';
 
 
-const Post = ({post,setCurrentId}) => {
-    const dispatch = useDispatch();
-    const classes = useStyles();
-    return (
-        <Card className={classes.card}>
+const Post = ({ post, setCurrentId }) => {
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
       <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
       <div className={classes.overlay}>
         <Typography variant="h6">{post.creator}</Typography>
@@ -32,10 +32,11 @@ const Post = ({post,setCurrentId}) => {
         <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
+        <Button size="small" color="secondary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" /> Like {post.likeCount}</Button>
         <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize="small" /> Delete</Button>
       </CardActions>
     </Card>
-    )
+  )
 }
 
 export default Post
